961cb3d9a227567aecc3ae9f2f959fc00bf536a1
Marlon Martins <marlon-martins@outlook.com>
2024-03-05 17:03:21 +0100

    Solution: Your first component

---
+++ Week 1/1- React Components and Where They Live/my-component/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Heading from './components/Heading';
+
+function App() {
+  return (
+    <div className="App">
+      This is the starting code for "Your first component" ungraded lab
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week 1/1- React Components and Where They Live/my-component/src/components/Heading.jsx
@@ -0,0 +1,5 @@
+export default function Heading() {
+  return (
+    <h1>This is an h1 heading</h1>
+  )
+}
