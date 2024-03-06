741259ac9db02fabb602b0810d5802a09eeedfa8
Marlon Martins <marlon-martins@outlook.com>
2024-03-06 17:02:57 +0100

    Solution: Creating and importing components

--- Week 1/1- React Components and Where They Live/my-component/src/App.js
+++ Week 1/1- React Components and Where They Live/my-component/src/App.js
@@ -4,7 +4,6 @@ import Heading from './components/Heading';
 function App() {
   return (
     <div className="App">
-      This is the starting code for "Your first component" ungraded lab
       <Heading />
     </div>
   );
