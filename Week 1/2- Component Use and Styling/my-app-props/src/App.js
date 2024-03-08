83be7e42b7824ea174c725a3088dc319fe233a56
Marlon Martins <marlon-martins@outlook.com>
2024-03-08 11:09:11 +0100

    Solution: Multiple components

--- Week 1/2- Component Use and Styling/my-app-props/src/App.js
+++ Week 1/2- Component Use and Styling/my-app-props/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Card from './components/Card';
 import Heading from './components/Heading';
 
 function App() {
@@ -7,6 +8,10 @@ function App() {
       <Heading firstName='Bob' />
       <Heading firstName='Any name other than Bob' />
       <Heading firstName='Jack' />
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2"/>
+      <Card h3="First card's h3"/>
+      <Card h2="Second cards h2" h3="Second cards h3"/>
     </div>
   );
 }
